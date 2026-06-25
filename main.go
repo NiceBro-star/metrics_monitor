789ab9da7cc5086bf53e3d981acaac875314b165
ldy <ldy1518@hotmail.com>
2026-06-25 16:16:03 +0800

    range hostList

--- main.go
+++ main.go
@@ -11,18 +11,14 @@ func main() {
 	host3 := api.HOST3
 	host4 := api.HOST4
 
-	monitorRes1 := api.MonitorTemplate1(host1)
-	monitorRes2 := api.MonitorTemplate1(host2)
-	monitorRes3 := api.MonitorTemplate1(host3)
-	monitorRes4 := api.MonitorTemplate1(host4)
+	hostList := make([]api.HOSTINFO, 0, 4)
+	hostList = append(hostList, host1, host2, host3, host4)
 
 	fmt.Println("IP | LB | CPU | MEM")
-	fmt.Print(host1.Host + ": ")
-	fmt.Println(monitorRes1)
-	fmt.Print(host2.Host + ": ")
-	fmt.Println(monitorRes2)
-	fmt.Print(host3.Host + ": ")
-	fmt.Println(monitorRes3)
-	fmt.Print(host4.Host + ": ")
-	fmt.Println(monitorRes4)
+	for _, host := range hostList {
+		monitorRes := api.MonitorTemplate1(host)
+
+		fmt.Print(host.Host + ": ")
+		fmt.Println(monitorRes)
+	}
 }
